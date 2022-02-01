4c590cc73fc85d590d114ff8568f101ca73ad6c9
Oliver <409038960@qq.com>
2022-02-02 05:13:14 +0800

    change comments to english

--- server/socketIO/index.js
+++ server/socketIO/index.js
@@ -1,18 +1,18 @@
 const {ChatModel} = require("../db/models");
 
 module.exports = function (server) {
-  // 得到IO 对象
+  // get IO object
   const io = require("socket.io")(server, {
     cors: {
       origin: "*",
     },
   });
-  // 监视连接(当有一个客户连接上时回调)
+  // monitor connection
   io.on("connection", function (socket) {
     console.log("soketio connected");
-    // 绑定sendMsg 监听, 接收客户端发送的消息
+    // binding sendMsg listening, and receive message from client
     socket.on("sendMsg", function ({ from, to, content }) {
-      console.log("服务器接收到浏览器的消息", { from, to, content });
+      console.log("server receive msg from client", { from, to, content });
 
       const chat_id = [from, to].sort().join("_");
       const create_time = Date.now();
@@ -21,7 +21,7 @@ module.exports = function (server) {
         chatMsg
       ) {
         io.emit("receiveMsg", chatMsg);
-        console.log("服务器向浏览器发送消息", chatMsg);
+        console.log("server send msg to client", chatMsg);
       });
     });
   });
